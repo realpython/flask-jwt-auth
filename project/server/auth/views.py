574b0fe678aca7f75dc1fbddc7a7ae1374ad53c2
d3prof3t <saurabhpysharma@gmail.com>
2017-01-05 14:29:54 +0530

    some more tests

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -118,7 +118,7 @@ class UserAPI(MethodView):
                 'status': 'fail',
                 'message': resp
             }
-            return make_response(jsonify(responseObject)), 200
+            return make_response(jsonify(responseObject)), 401
         else:
             responseObject = {
                 'status': 'fail',
@@ -169,7 +169,7 @@ class LogoutAPI(MethodView):
                 'status': 'fail',
                 'message': 'Provide a valid auth token.'
             }
-            return make_response(jsonify(responseObject)), 403
+            return make_response(jsonify(responseObject)), 401
 
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
