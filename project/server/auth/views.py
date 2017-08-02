359a5bb9bc3a23d53e35b45f96c2fceb6d3c2afd
Michael Herman <hermanmu@gmail.com>
2017-08-02 08:52:50 -0600

    added exception to splitting of auth token

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -97,7 +97,14 @@ class UserAPI(MethodView):
         # get the auth token
         auth_header = request.headers.get('Authorization')
         if auth_header:
-            auth_token = auth_header.split(" ")[1]
+            try:
+                auth_token = auth_header.split(" ")[1]
+            except IndexError:
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Bearer token malformed.'
+                }
+                return make_response(jsonify(responseObject)), 401
         else:
             auth_token = ''
         if auth_token:
