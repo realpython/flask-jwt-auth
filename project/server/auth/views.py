15f6c026e169e99e1b216951087c0b07e346bedb
Tyler Renelle <tylerrenelle@gmail.com>
2017-08-03 09:40:31 -0700

    return 401 instead of 403 on missing token

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -169,7 +169,7 @@ class LogoutAPI(MethodView):
                 'status': 'fail',
                 'message': 'Provide a valid auth token.'
             }
-            return make_response(jsonify(responseObject)), 403
+            return make_response(jsonify(responseObject)), 401
 
 # define the API resources
 registration_view = RegisterAPI.as_view('register_api')
